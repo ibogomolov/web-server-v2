8fbbe28ec18bb6b62878cc9fc5f44ce24087dc2e
Bogomolov, Igor <igor.bogomolov@gmail.com>
2025-10-07 23:38:49 +0200

    feat: implement FileHandler

--- src/main/java/com/example/ibogomolov/FileHandler.java
+++ src/main/java/com/example/ibogomolov/FileHandler.java
@@ -4,8 +4,11 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import lombok.AllArgsConstructor;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.nio.file.Files;
 
 @AllArgsConstructor
 public class FileHandler implements HttpHandler {
@@ -16,7 +19,19 @@ public class FileHandler implements HttpHandler {
     public void handle(HttpExchange exchange) throws IOException {
         String relativePath = exchange.getRequestURI().getPath();
         IO.println(String.format("Accessing %s%s", this.rootDir, relativePath));
-        exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
-        exchange.close();
+        File file = new File(this.rootDir, relativePath);
+        if (file.exists() && file.isFile() && file.canRead()) {
+            exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, file.length());
+            try (OutputStream output = exchange.getResponseBody()) {
+                Files.copy(file.toPath(), output);
+            } catch (IOException e) {
+                System.err.println("Error reading file " + file.getAbsolutePath());
+                exchange.sendResponseHeaders(HttpURLConnection.HTTP_INTERNAL_ERROR, 0);
+                exchange.close();
+            }
+        } else {
+            exchange.sendResponseHeaders(HttpURLConnection.HTTP_NOT_FOUND, 0);
+            exchange.close();
+        }
     }
 }
