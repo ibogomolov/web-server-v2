47692b25c21a5f6a8edd9c950806a42e621da1e7
Bogomolov, Igor <igor.bogomolov@gmail.com>
2025-10-08 01:56:07 +0200

    feat: set content type and allow only get

--- src/main/java/com/example/ibogomolov/FileHandler.java
+++ src/main/java/com/example/ibogomolov/FileHandler.java
@@ -1,19 +1,37 @@
 package com.example.ibogomolov;
 
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpHandlers;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.URLConnection;
 import java.nio.file.Files;
+import java.util.List;
 
-@AllArgsConstructor
-public class FileHandler implements HttpHandler {
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public final class FileHandler implements HttpHandler {
+
+    public static final List<String> ALLOWED_METHODS = List.of("GET");
 
     private String rootDir;
+
+    /**
+     * Inspired by the similar one in sun.net.httpserver.simpleserver.FileServerHandler.
+     */
+    public static HttpHandler create(String rootDir) {
+        HttpHandler badMethodHandler = HttpHandlers.of(HttpURLConnection.HTTP_BAD_METHOD,
+                Headers.of("Allow", "GET"), "");
+        return HttpHandlers.handleOrElse(r -> ALLOWED_METHODS.contains(r.getRequestMethod()),
+                new FileHandler(rootDir), badMethodHandler);
+    }
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
@@ -21,17 +39,24 @@ public class FileHandler implements HttpHandler {
         IO.println(String.format("Accessing %s%s", this.rootDir, relativePath));
         File file = new File(this.rootDir, relativePath);
         if (file.exists() && file.isFile() && file.canRead()) {
+            FileHandler.setContentType(file, exchange);
             exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, file.length());
             try (OutputStream output = exchange.getResponseBody()) {
                 Files.copy(file.toPath(), output);
             } catch (IOException e) {
                 System.err.println("Error reading file " + file.getAbsolutePath());
-                exchange.sendResponseHeaders(HttpURLConnection.HTTP_INTERNAL_ERROR, 0);
+                exchange.sendResponseHeaders(HttpURLConnection.HTTP_INTERNAL_ERROR, -1);
                 exchange.close();
             }
         } else {
-            exchange.sendResponseHeaders(HttpURLConnection.HTTP_NOT_FOUND, 0);
+            exchange.sendResponseHeaders(HttpURLConnection.HTTP_NOT_FOUND, -1);
             exchange.close();
         }
+    }
+
+    private static void setContentType(File file, HttpExchange exchange) {
+        String mimeType = URLConnection.guessContentTypeFromName(file.getName());
+        exchange.getResponseHeaders().add("Content-Type", mimeType);
+        IO.println(exchange.getResponseHeaders());
     }
 }
--- src/main/java/com/example/ibogomolov/Main.java
+++ src/main/java/com/example/ibogomolov/Main.java
@@ -1,18 +1,24 @@
-import com.example.ibogomolov.CustomThreadPoolExecutor;
-import com.example.ibogomolov.FileHandler;
+package com.example.ibogomolov;
+
 import com.sun.net.httpserver.HttpServer;
 
-public static final int THREADS_MAX_COUNT = 5;
+import java.io.IOException;
+import java.net.InetSocketAddress;
 
-void main() {
-    try {
-        HttpServer server = HttpServer.create();
-        server.bind(new InetSocketAddress(80), 0);
-        server.createContext("/", new FileHandler("./web-root"));
-        server.setExecutor(new CustomThreadPoolExecutor());
-        server.start();
-        IO.println("The file server is started.");
-    } catch (IOException e) {
-        System.err.println("Error while starting the file server: " + e);
+
+public class Main {
+    public static final int THREADS_MAX_COUNT = 5;
+
+    static void main(String[] args) {
+        try {
+            HttpServer server = HttpServer.create();
+            server.bind(new InetSocketAddress(80), 0);
+            server.createContext("/", FileHandler.create("./dev-web-root"));
+            server.setExecutor(new CustomThreadPoolExecutor());
+            server.start();
+            IO.println("The file server is started.");
+        } catch (IOException e) {
+            System.err.println("Error while starting the file server: " + e);
+        }
     }
 }
